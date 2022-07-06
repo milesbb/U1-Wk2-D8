b78503512e25c8eede56ce98827a21751ae8f339
Miles Johann Bailey-Braendgaard <milesjohannbailey-braendgaard@miless-air.home>
2022-07-06 14:47:05 +0100

    Part 1 finished

--- part1.js
+++ part1.js
@@ -8,35 +8,53 @@ ASSIGNMENT RULES
 - The solution must be available for the tutors by the end of the day (5PM CET)
 */
 
+console.log("=====================Part 1=====================")
+
 /* EXERCISE 1
  Create a variable and assign to it an array containing the first 5 positive numbers.
 */
 
-/* WRITE YOUR ANSWER HERE */
+console.log("\n==========Question 1==========")
+let q1Array = [1, 2, 3, 4, 5];
+console.log(q1Array.toString());
 
 /* EXERCISE 2
  Create a variable and assign to it an object containing your name, surname, email address and age.
 */
 
-/* WRITE YOUR ANSWER HERE */
+console.log("\n==========Question 2==========")
+let person = {
+    firstname : "Miles",
+    lastname : "Bailey-Braendgaard",
+    email : "xxx@xxx.com",
+    age : 22,
+}
+console.log(person);
 
 /* EXERCISE 3
  Add to the previously created object a property with a boolean value to rappresent wheter you have or not a driving license.
 */
 
-/* WRITE YOUR ANSWER HERE */
+console.log("\n==========Question 3==========")
+person.drivingLicense = true;
+console.log(person);
 
 /* EXERCISE 4
  Remove from the previously created object the age property.
 */
 
-/* WRITE YOUR ANSWER HERE */
+console.log("\n==========Question 4==========")
+delete person.age;
+console.log(person);
 
 /* EXERCISE 5
  Create a second object with another name, surname, email address and verify that this object has a different email address than the previous one.
 */
 
-/* WRITE YOUR ANSWER HERE */
+console.log("\n==========Question 5==========")
+let secondPerson = Object.assign({}, person, {email : "yyy@yyy.com"})
+console.log(person.email);
+console.log(secondPerson.email);
 
 /* EXERCISE 6
  You are working on an e-commerce website. In the variable totalShoppingCart you are storing the total amount spent by the current user.
@@ -44,49 +62,101 @@ ASSIGNMENT RULES
  Write an algorithm that calculates the total cost to charge the user with.
 */
 
-/* WRITE YOUR ANSWER HERE */
+console.log("\n==========Question 6==========")
+let totalShoppingCart = 12;
+let totalCost = totalShoppingCart;
+if (totalShoppingCart <= 50) {
+    totalCost += 10;
+}
+
+console.log(`Your basket total is ${totalShoppingCart} The total cost is ${totalCost}`);
 
 /* EXERCISE 7
  You are working on an e-commerce website. Today is Black Friday and everything has a 20% discount at the end of the purchase.
  Modify the previous answer inserting this information and, applying the same rules for the shipping cost, calculate the totalCost.
 */
 
-/* WRITE YOUR ANSWER HERE */
+console.log("\n==========Question 7==========")
+totalShoppingCart = 12;
+totalShoppingCart *= 0.8;
+if (totalShoppingCart <= 50) {
+    totalCost += 10;
+}
+
+console.log(`Your basket total is ${totalShoppingCart.toFixed(2)} with a 20% discount applied. 
+\nThe total cost is ${totalCost}`);
 
 /* EXERCISE 8
  Create a variable and assign to it an object representing a car, with properties like brand, model and licensePlate.
  Then clone it 5 times, and change the licensePlate for each cloned car without affecting the original one.
 */
 
-/* WRITE YOUR ANSWER HERE */
+console.log("\n==========Question 8==========")
+let car1 = {
+    brand : "Aston Fartin",
+    model : "12-22GX",
+    licensePlate : "WY2 3FG"
+};
+
+car2 = Object.assign({}, car1, {licensePlate : "23C FSE"});
+car3 = Object.assign({}, car1, {licensePlate : "D20 DSG"});
+car4 = Object.assign({}, car1, {licensePlate : "WII 902"});
+car5 = Object.assign({}, car1, {licensePlate : "GSL FUU"});
+car6 = Object.assign({}, car1, {licensePlate : "SLA 54F"});
+
+console.log(`Car liscence plates: ${car1.licensePlate}, ${car2.licensePlate}, ${car3.licensePlate}, ${car4.licensePlate}, ${car5.licensePlate}, ${car6.licensePlate}`)
 
 /* EXERCISE 9
  Create a variable called carsForRent and assign to it an array containing all the cars from the previous exercise.
 */
 
-/* WRITE YOUR ANSWER HERE */
+console.log("\n==========Question 9==========");
+
+let carsForRent = [car1, car2, car3, car4, car5, car6];
+console.log(carsForRent);
 
 /* EXERCISE 10
  Remove the first and the last car from the carsForRent array.
 */
 
-/* WRITE YOUR ANSWER HERE */
+console.log("\n==========Question 10==========");
+
+carsForRent.splice(0, 1);
+carsForRent.splice((carsForRent.length - 1), 1);
+console.log(carsForRent);
 
 /* EXERCISE 11
  Print to the console the type of the car variable you created before, as well as the types of its licensePlate and brand properties.
 */
 
-/* WRITE YOUR ANSWER HERE */
+console.log("\n==========Question 11==========");
+
+console.log(typeof car1);
+console.log(typeof car1.licensePlate);
+console.log(typeof car1.brand);
 
 /* EXERCISE 12
  Create a new variable called carsForSale assigning to it an empty array, and then insert 3 cars into it.
  Create a new variable called totalCars and assign to it the total number of cars present in the carsForSale and carsForRent arrays.
 */
 
-/* WRITE YOUR ANSWER HERE */
+console.log("\n==========Question 12==========");
+
+let carsForSale = Array(3);
+
+carsForSale[0] = car1;
+carsForSale[1] = car2;
+carsForSale[2] = car3;
+
+let totalCars = carsForSale.length + carsForRent.length;
+console.log(totalCars.toString())
 
 /* EXERCISE 13
  Using a loop, print to the console all the data for each car in the carsForSale array.
 */
 
-/* WRITE YOUR ANSWER HERE */
+console.log("\n==========Question 13==========");
+
+for (let i = 0; i < carsForSale.length; i++) {
+    console.log(carsForSale[i])
+}
--- part2.js
+++ part2.js
@@ -1,5 +1,5 @@
 // Additional assignments for Day 4
-console.log("=====================Part 1=====================")
+console.log("=====================Part 2=====================")
 /* EXTRA 1
  Write a piece of code for reverting an array.
  es:
@@ -7,46 +7,68 @@ console.log("=====================Part 1=====================")
 */
 
 console.log("\n==========Question 1==========")
-let q1array = [1, 3, 5];
-console.log(q1array.toString());
-q1array = q1array.reverse();
-console.log(q1array.toString())
+let q1Array = [1, 3, 5];
+console.log(q1Array.toString());
+q1Array = q1Array.reverse();
+console.log(q1Array.toString())
 
 /* EXTRA 2
  Write a piece of code for getting the maximum numerical value from an array.
 */
 
 console.log("\n==========Question 2==========")
-let maxnumber = Math.max(...q1array);
-console.log(maxnumber.toString());
+let maxNumber = Math.max(...q1Array);
+console.log(maxNumber.toString());
 
 /* EXTRA 3
  Write a piece of code for getting the minimum numerical value from an array.
 */
 
 console.log("\n==========Question 3==========")
-let minnumber = Math.min(...q1array);
-console.log(minnumber.toString());
+let minNumber = Math.min(...q1Array);
+console.log(minNumber.toString());
 
 /* EXTRA 4
  Write a piece of code for getting only even numerical values from an array.
 */
 
 console.log("\n==========Question 4==========")
-let evennumber = Math.(...q1array);
-console.log(evennumber.toString());
+let q4Array = [1, 4, 6, 3, 2, 10, 7]
+let even = q4Array.filter(number => {
+    return number % 2 === 0;
+});
+console.log(even.toString());
 
 /* EXTRA 5
  Write a piece of code for deleting only even entries from an array.
 */
 
-/* WRITE YOUR ANSWER HERE */
+console.log("\n==========Question 5==========");
+let q5Array = [1, 4, 6, 3, 2, 10, 7];
+for (let i = 0; i < q5Array.length; i++) {
+    if (q5Array[i] % 2 === 0) {
+        q5Array.splice(i, 1);
+        i -= 1
+    }
+}
+console.log(q5Array.toString());
 
 /* EXTRA 6
  Write a piece of code for removing all the vowels from a string.
 */
 
-/* WRITE YOUR ANSWER HERE */
+console.log("\n==========Question 6==========");
+let vowels = new RegExp("aeiou", "gi"); // ["a", "e", "i", "o", "u"];
+let q6String = "ABC abc DEF def AAA aaa BBB bbb CCC ccc";
+let letters = q6String.split("");
+for (let i = 0; i < letters.length; i++) {
+    if (letters[i] === vowels) {
+        letters.splice(i, 1);
+        i -= 1
+    }
+}
+q6String = letters.join("");
+console.log(q6String);
 
 /* EXTRA 7
  Write a piece of code for increasing all the numerical values in a array by 1.
