85f9dbff87de4c93ca6e504ceb1b94939640b6ea
Miles Johann Bailey-Braendgaard <milesjohannbailey-braendgaard@miless-air.home>
2022-07-06 13:22:07 +0100

    Names switched

--- part1.js
+++ part1.js
@@ -1,52 +1,92 @@
-// Additional assignments for Day 4
+/*
+ASSIGNMENT RULES
+- All the answers must be written in JavaScript
+- You can ask for help, reach the Teaching Assistants if needed
+- You can Google / use StackOverflow BUT only when you think you need something we didn't cover during lessons yet
+- You can test your code in a separate file or de-commenting the single exercises in this one.
+- You can use the bash terminal, the VSCode terminal or the one embedded in your Operating System if you're using macOS or Linux.
+- The solution must be available for the tutors by the end of the day (5PM CET)
+*/
 
-/* EXTRA 1
- Write a piece of code for reverting an array.
- es:
- [1, 3, 5] ==> [5, 3, 1]
+/* EXERCISE 1
+ Create a variable and assign to it an array containing the first 5 positive numbers.
 */
 
 /* WRITE YOUR ANSWER HERE */
 
-/* EXTRA 2
- Write a piece of code for getting the maximum numerical value from an array.
+/* EXERCISE 2
+ Create a variable and assign to it an object containing your name, surname, email address and age.
 */
 
 /* WRITE YOUR ANSWER HERE */
 
-/* EXTRA 3
- Write a piece of code for getting the minimum numerical value from an array.
+/* EXERCISE 3
+ Add to the previously created object a property with a boolean value to rappresent wheter you have or not a driving license.
 */
 
 /* WRITE YOUR ANSWER HERE */
 
-/* EXTRA 4
- Write a piece of code for getting only even numerical values from an array.
+/* EXERCISE 4
+ Remove from the previously created object the age property.
 */
 
 /* WRITE YOUR ANSWER HERE */
 
-/* EXTRA 5
- Write a piece of code for deleting only even entries from an array.
+/* EXERCISE 5
+ Create a second object with another name, surname, email address and verify that this object has a different email address than the previous one.
 */
 
 /* WRITE YOUR ANSWER HERE */
 
-/* EXTRA 6
- Write a piece of code for removing all the vowels from a string.
+/* EXERCISE 6
+ You are working on an e-commerce website. In the variable totalShoppingCart you are storing the total amount spent by the current user.
+ Currently you have a promotion: if the customer's shopping cart total is more than 50, the user is eligible for free shipping (otherwise it costs 10).
+ Write an algorithm that calculates the total cost to charge the user with.
 */
 
 /* WRITE YOUR ANSWER HERE */
 
-/* EXTRA 7
- Write a piece of code for increasing all the numerical values in a array by 1.
+/* EXERCISE 7
+ You are working on an e-commerce website. Today is Black Friday and everything has a 20% discount at the end of the purchase.
+ Modify the previous answer inserting this information and, applying the same rules for the shipping cost, calculate the totalCost.
 */
 
 /* WRITE YOUR ANSWER HERE */
 
-/* EXTRA 8 
- Replace all the strings contained in an array with their length.
- es.: ["strive", "is", "great"] => [6, 2, 5]
+/* EXERCISE 8
+ Create a variable and assign to it an object representing a car, with properties like brand, model and licensePlate.
+ Then clone it 5 times, and change the licensePlate for each cloned car without affecting the original one.
+*/
+
+/* WRITE YOUR ANSWER HERE */
+
+/* EXERCISE 9
+ Create a variable called carsForRent and assign to it an array containing all the cars from the previous exercise.
+*/
+
+/* WRITE YOUR ANSWER HERE */
+
+/* EXERCISE 10
+ Remove the first and the last car from the carsForRent array.
+*/
+
+/* WRITE YOUR ANSWER HERE */
+
+/* EXERCISE 11
+ Print to the console the type of the car variable you created before, as well as the types of its licensePlate and brand properties.
+*/
+
+/* WRITE YOUR ANSWER HERE */
+
+/* EXERCISE 12
+ Create a new variable called carsForSale assigning to it an empty array, and then insert 3 cars into it.
+ Create a new variable called totalCars and assign to it the total number of cars present in the carsForSale and carsForRent arrays.
+*/
+
+/* WRITE YOUR ANSWER HERE */
+
+/* EXERCISE 13
+ Using a loop, print to the console all the data for each car in the carsForSale array.
 */
 
 /* WRITE YOUR ANSWER HERE */
--- part2.js
+++ part2.js
@@ -1,92 +1,62 @@
-/*
-ASSIGNMENT RULES
-- All the answers must be written in JavaScript
-- You can ask for help, reach the Teaching Assistants if needed
-- You can Google / use StackOverflow BUT only when you think you need something we didn't cover during lessons yet
-- You can test your code in a separate file or de-commenting the single exercises in this one.
-- You can use the bash terminal, the VSCode terminal or the one embedded in your Operating System if you're using macOS or Linux.
-- The solution must be available for the tutors by the end of the day (5PM CET)
+// Additional assignments for Day 4
+console.log("=====================Part 1=====================")
+/* EXTRA 1
+ Write a piece of code for reverting an array.
+ es:
+ [1, 3, 5] ==> [5, 3, 1]
 */
 
-/* EXERCISE 1
- Create a variable and assign to it an array containing the first 5 positive numbers.
+console.log("\n==========Question 1==========")
+let q1array = [1, 3, 5];
+console.log(q1array.toString());
+q1array = q1array.reverse();
+console.log(q1array.toString())
+
+/* EXTRA 2
+ Write a piece of code for getting the maximum numerical value from an array.
 */
 
-/* WRITE YOUR ANSWER HERE */
+console.log("\n==========Question 2==========")
+let maxnumber = Math.max(...q1array);
+console.log(maxnumber.toString());
 
-/* EXERCISE 2
- Create a variable and assign to it an object containing your name, surname, email address and age.
+/* EXTRA 3
+ Write a piece of code for getting the minimum numerical value from an array.
 */
 
-/* WRITE YOUR ANSWER HERE */
+console.log("\n==========Question 3==========")
+let minnumber = Math.min(...q1array);
+console.log(minnumber.toString());
 
-/* EXERCISE 3
- Add to the previously created object a property with a boolean value to rappresent wheter you have or not a driving license.
+/* EXTRA 4
+ Write a piece of code for getting only even numerical values from an array.
 */
 
-/* WRITE YOUR ANSWER HERE */
+console.log("\n==========Question 4==========")
+let evennumber = Math.(...q1array);
+console.log(evennumber.toString());
 
-/* EXERCISE 4
- Remove from the previously created object the age property.
-*/
-
-/* WRITE YOUR ANSWER HERE */
-
-/* EXERCISE 5
- Create a second object with another name, surname, email address and verify that this object has a different email address than the previous one.
-*/
-
-/* WRITE YOUR ANSWER HERE */
-
-/* EXERCISE 6
- You are working on an e-commerce website. In the variable totalShoppingCart you are storing the total amount spent by the current user.
- Currently you have a promotion: if the customer's shopping cart total is more than 50, the user is eligible for free shipping (otherwise it costs 10).
- Write an algorithm that calculates the total cost to charge the user with.
+/* EXTRA 5
+ Write a piece of code for deleting only even entries from an array.
 */
 
 /* WRITE YOUR ANSWER HERE */
 
-/* EXERCISE 7
- You are working on an e-commerce website. Today is Black Friday and everything has a 20% discount at the end of the purchase.
- Modify the previous answer inserting this information and, applying the same rules for the shipping cost, calculate the totalCost.
+/* EXTRA 6
+ Write a piece of code for removing all the vowels from a string.
 */
 
 /* WRITE YOUR ANSWER HERE */
 
-/* EXERCISE 8
- Create a variable and assign to it an object representing a car, with properties like brand, model and licensePlate.
- Then clone it 5 times, and change the licensePlate for each cloned car without affecting the original one.
+/* EXTRA 7
+ Write a piece of code for increasing all the numerical values in a array by 1.
 */
 
 /* WRITE YOUR ANSWER HERE */
 
-/* EXERCISE 9
- Create a variable called carsForRent and assign to it an array containing all the cars from the previous exercise.
-*/
-
-/* WRITE YOUR ANSWER HERE */
-
-/* EXERCISE 10
- Remove the first and the last car from the carsForRent array.
-*/
-
-/* WRITE YOUR ANSWER HERE */
-
-/* EXERCISE 11
- Print to the console the type of the car variable you created before, as well as the types of its licensePlate and brand properties.
-*/
-
-/* WRITE YOUR ANSWER HERE */
-
-/* EXERCISE 12
- Create a new variable called carsForSale assigning to it an empty array, and then insert 3 cars into it.
- Create a new variable called totalCars and assign to it the total number of cars present in the carsForSale and carsForRent arrays.
-*/
-
-/* WRITE YOUR ANSWER HERE */
-
-/* EXERCISE 13
- Using a loop, print to the console all the data for each car in the carsForSale array.
+/* EXTRA 8 
+ Replace all the strings contained in an array with their length.
+ es.: ["strive", "is", "great"] => [6, 2, 5]
 */
 
 /* WRITE YOUR ANSWER HERE */
